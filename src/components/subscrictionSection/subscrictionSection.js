143d53c48f392c72c806022d0f83fb1cf6dbdeff
Riya Regmi <riyaregmi19@gmail.com>
2025-01-24 19:06:47 -0500

    funfact section

--- src/components/subscrictionSection/subscrictionSection.js
+++ src/components/subscrictionSection/subscrictionSection.js
@@ -8,8 +8,32 @@ export const SubscrictionSection = () => {
     <div className="Subscriction-Section-layout">
       <div className="row Subscriction-Section-content">
         <div className="col-4">
-          <Card title="Card title" bordered={false}>
-            Card content
+          <Card  bordered={false}>
+            <div className="subscription-section-card-content">
+              <div className="subscription-section-card-title">
+                <div>
+                  <span className="subscription-section-card-title-price">$0.00</span>
+                  <br/>
+                  <span>Free</span>
+                </div>
+                
+              </div>
+              <ul>
+                <li>Lorem ipsum dolor sit amet.
+                </li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+              </ul>
+              <div className="subscription-section-card-button-section">
+                <span>
+                  <button className="black-button">Choose A Plan</button>
+                </span>
+              </div>
+            </div>
           </Card>
         </div>
         <div className="col-4">

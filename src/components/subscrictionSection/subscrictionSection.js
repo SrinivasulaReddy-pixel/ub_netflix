a2bb1f889a89835ff601c1329547ae5baed8c877
1217839 <balusrinivasulareddy@gmail.com>
2025-02-01 12:21:20 -0500

    created cards for subscription pages

--- src/components/subscrictionSection/subscrictionSection.js
+++ src/components/subscrictionSection/subscrictionSection.js
@@ -37,14 +37,62 @@ export const SubscrictionSection = () => {
           </Card>
         </div>
         <div className="col-4">
-        <Card title="Card title" bordered={false}>
-          Card content
+        <Card bordered={false}>
+        <div className="subscription-section-card-content">
+              <div className="subscription-section-card-title-2">
+                <div>
+                  <span className="subscription-section-card-title-price-2">$100.00</span>
+                  <br/>
+                  <span>Premium</span>
+                </div>
+                
+              </div>
+              <ul>
+                <li>Lorem ipsum dolor sit amet.
+                </li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+              </ul>
+              <div className="subscription-section-card-button-section-2">
+                <span>
+                  <button className="black-button-2">Choose A Plan</button>
+                </span>
+              </div>
+            </div>
         </Card>
         </div>
         
         <div className="col-4">
-          <Card title="Card title" bordered={false}>
-            Card content
+          <Card bordered={false}>
+          <div className="subscription-section-card-content">
+              <div className="subscription-section-card-title">
+                <div>
+                  <span className="subscription-section-card-title-price">$200.00</span>
+                  <br/>
+                  <span>Standard</span>
+                </div>
+                
+              </div>
+              <ul>
+                <li>Lorem ipsum dolor sit amet.
+                </li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+              </ul>
+              <div className="subscription-section-card-button-section">
+                <span>
+                  <button className="black-button">Choose A Plan</button>
+                </span>
+              </div>
+            </div>
           </Card>
         </div>
       </div>
